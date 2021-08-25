695756a3b47afe2d9585e99b9c7cdcfe79035597
Liliana797979 <76112004+Liliana797979@users.noreply.github.com>
2021-08-25 11:42:04 +0300

    Functions - lab
    JS fundamentals module

---
+++ Functions - lab/06.SignCheck.js
@@ -0,0 +1,17 @@
+function signCheck(numOne, numTwo, numThree) {
+    if (numOne >= 0 && numTwo >= 0 && numThree >= 0 ) {
+        console.log("Positive");
+    } else if (numOne <= 0 && numTwo <= 0 && numThree >= 0) {
+        console.log("Positive");
+    } else if (numOne >= 0 && numTwo <= 0 && numThree <= 0) {
+        console.log("Positive");
+    } else if (numOne <= 0 && numTwo >= 0 && numThree <= 0) {
+        console.log("Positive");
+    } else if (numOne >= 0 && numTwo >= 0 && numThree <= 0) {
+        console.log("Negative");
+    } else if (numOne <= 0 && numTwo <= 0 && numThree <= 0) {
+        console.log("Negative");
+    } else if(numOne <= 0 && numTwo >= 0 && numThree >= 0) {
+        console.log("Negative");
+    }
+}
